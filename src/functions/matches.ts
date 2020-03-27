36b4b3a54d268c14e56c5308628493db6e4e0421
Yomotsu <admin@yomotsu.net>
2020-03-27 20:36:29 +0900

    webkit maches is not needed

--- src/functions/matches.ts
+++ src/functions/matches.ts
@@ -12,12 +12,6 @@ export function matches( el: Element, selector: string ): boolean {
 
 	}
 
-	if ( !! Element.prototype.webkitMatchesSelector ) {
-
-		return el.webkitMatchesSelector( selector );
-
-	}
-
 	if ( !! Element.prototype.msMatchesSelector ) {
 
 		return el.msMatchesSelector( selector );
