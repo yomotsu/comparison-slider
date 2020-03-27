54c08da45b9556b7560c5819d2420d9f738a31fe
Yomotsu <admin@yomotsu.net>
2020-03-27 20:32:21 +0900

    improve handle-only handler

---
+++ src/functions/matches.ts
@@ -0,0 +1,29 @@
+declare global {
+	interface Element {
+		msMatchesSelector( selectors: string ): boolean;
+	}
+}
+
+export function matches( el: Element, selector: string ): boolean {
+
+	if ( !! Element.prototype.matches ) {
+
+		return el.matches( selector );
+
+	}
+
+	if ( !! Element.prototype.webkitMatchesSelector ) {
+
+		return el.webkitMatchesSelector( selector );
+
+	}
+
+	if ( !! Element.prototype.msMatchesSelector ) {
+
+		return el.msMatchesSelector( selector );
+
+	}
+
+	return false;
+
+}
